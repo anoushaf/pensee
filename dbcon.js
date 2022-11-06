bbbcaadbc5b4c8653ac34754ec2ab1258d69f42f
anoushaf <anousha.kf@gmail.com>
2022-11-06 01:22:53 -0700

    deleted flightplan

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimit: 10,
   database: "pensee",
-  host: '10.0.0.79',
+  host: '188.166.50.188',
   multipleStatements: true,
   password: "12345678",
   user: "pensee"
