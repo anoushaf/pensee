e91240fefd3d58e99b715cbe74a3d555aac7e4d4
anoushaf <anousha.kf@gmail.com>
2022-11-09 21:52:49 -0800

    IP

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
   connectionLimit: 10,
   database: "pensee",
-  host: '188.166.50.188',
+  host: '137.184.184.246',
   multipleStatements: true,
   password: "12345678",
   user: "pensee"
