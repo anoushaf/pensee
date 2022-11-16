818931a1270b787a2dbaeb0418af7b4b14a4b498
anoushaf <anousha.kf@gmail.com>
2022-11-15 18:37:48 -0800

    added margin

--- dbcon.js
+++ dbcon.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  database: "pensee",
-  host: '137.184.184.246',
+  database: 'pensee',
+  host: '127.0.0.1',
   multipleStatements: true,
-  password: "12345678",
-  user: "pensee"
+  password: 'music22?',
+  user: 'root'
 });
 
 module.exports.pool = pool;
